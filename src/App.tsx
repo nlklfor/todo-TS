f6ff7d545fc4b9711c6e92276927f2448ba83627
Nikita <nik.slepcov1771@gmail.com>
2024-01-21 18:23:47 +0300

    error field and active-btn added

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,79 @@
 import React, {useState} from 'react';
-import './App.css';
+import './App.scss';
 import Todo from "./components/Todo";
+import {v1} from "uuid";
 
 
 function App() {
     const [task, setTask] = useState([
-        {id: 1, title: 'Learn HTML', isChecked: true},
-        {id: 2, title: 'Learn TS', isChecked: false},
-        {id: 3, title: 'Learn ReactJS', isChecked: true},
-        {id: 4, title: 'Learn MobX', isChecked: false},
-        {id: 5, title: 'Learn CSS', isChecked: true},
-        {id: 6, title: 'Learn MaterialUI', isChecked: false},
-        {id: 7, title: 'Learn JS', isChecked: true},
+        {id: v1(), title: 'Learn HTML', isChecked: true},
+        {id: v1(), title: 'Learn TS', isChecked: false},
+        {id: v1(), title: 'Learn ReactJS', isChecked: true},
+        {id: v1(), title: 'Learn MobX', isChecked: false},
+        {id: v1(), title: 'Learn CSS', isChecked: true},
+        {id: v1(), title: 'Learn MaterialUI', isChecked: false},
+        {id: v1(), title: 'Learn JS', isChecked: true},
     ])
     const [filtered, setFiltered] = useState('all')
     const [taskTitle, setTaskTitle] = useState('')
 
-    const onChangeInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTaskTitle(e.target.value)
+
+
+    const onChangeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTaskTitle(event.target.value)
+        console.log(event.target.value);
+    }
+
+    const checkedTaskChange = (taskId: string) => {
+        setTask((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === taskId ? { ...task, isChecked: !task.isChecked } : task
+            )
+        );
+    };
+
+    const onClear = () => {
+        setTaskTitle('');
     }
 
     const onAddTask = () => {
-        let newTasks = [...task, taskTitle]
-        console.log(newTasks)
+        let tmpAdd = [...task];
+        tmpAdd.push({id: v1(), title: taskTitle, isChecked: false})
+        if (taskTitle.trim() !== '') {
+            setTask(tmpAdd);
+        }
+        onClear();
     }
 
     let tasksForTodo = task;
 
     if (filtered === 'completed') {
-        tasksForTodo = task.filter(item=> item.isChecked === true)
+        tasksForTodo = task.filter(item=> item.isChecked)
     }
     if (filtered === 'active') {
-        tasksForTodo = task.filter(item=> item.isChecked === false)
+        tasksForTodo = task.filter(item=> !item.isChecked)
     }
     if (filtered === 'all') {
-        tasksForTodo = task.filter(item=> (item.isChecked === true) || (item.isChecked === false))
+        tasksForTodo = task.filter(item=> item.isChecked || !item.isChecked)
     }
     const onComplete = () => {
         setFiltered('completed')
-        console.log(tasksForTodo)
+
     }
     const onActive = () => {
         setFiltered('active')
-        console.log(tasksForTodo)
+
     }
     const onAll = () => {
         setFiltered('all')
-        console.log(tasksForTodo)
+
     }
-    const onDeleteTask = (id: number) => {
+    const onDeleteTask = (id: string) => {
         setTask(task.filter( item=> item.id !== id))
     }
     return (
         <div className="App">
-            <Todo title={'Learn TS + ReactJS'} taskTitle={taskTitle} all={onAll} complete={onComplete} active={onActive} task={tasksForTodo} onChange={onChangeInputValue} onAdd={onAddTask} onDelete={onDeleteTask}/>
+            <Todo title={'Learn TS + ReactJS'} filtered={filtered} checkedTaskChange={checkedTaskChange} onClear={onClear} setTask={setTask} taskTitle={taskTitle} all={onAll} complete={onComplete} active={onActive} task={tasksForTodo} onChangeValue={onChangeInputValue} onAdd={onAddTask} onDelete={onDeleteTask}/>
         </div>
     );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,46 +1,82 @@
-import {useState} from "react";
+import {useEffect, MouseEvent, useState, MouseEventHandler} from "react";
+import './Todo.scss'
+
 
 type TaskType = {
-    id: number;
+    id: string;
     title: string,
     isChecked: boolean;
 }
 
 type PropsType = {
     title: string;
-    task: Array<TaskType>
-    onDelete: Function
-    onAdd: Function
-    onChange: Function
-    taskTitle: string
-    active: Function
-    complete: Function
-    all: Function
+    task: Array<TaskType>;
+    checkedTaskChange: (id: string) => void;
+    onDelete: Function;
+    onAdd: (event: MouseEvent<HTMLButtonElement>) => void;
+    onChangeValue: Function;
+    taskTitle: string;
+    active: (event: MouseEvent<HTMLButtonElement>) => void;
+    complete: (event: MouseEvent<HTMLButtonElement>) => void;
+    all: (event: MouseEvent<HTMLButtonElement>) => void;
+    setTask: Function;
+    filtered: string;
+    onClear: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-function Todo(props: PropsType){
-    return(
+
+function Todo(props: PropsType) {
+
+    const [error , setError] = useState<string | null>(null)
+
+    const onAddTaskHandle = (event: MouseEvent<HTMLButtonElement>) => {
+        props.onAdd(event);
+        if (props.taskTitle.trim() === '') {
+            setError(`This field can't be empty`);
+        } else {
+            setError(null);
+        }
+
+    }
+
+    useEffect(() => {
+        console.log('gizmo')
+    }, [])
+    
+    return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input onChange={() => {props.onChange()}} value={props.taskTitle}/>
-                <button onClick={() => {props.onAdd()}}>+</button>
+                {error && <p className={'error'}>{error}</p>}
+                <input className={`${error ? 'error-input' : 'input'}`} value={props.taskTitle} onChange={(event) =>
+                    props.onChangeValue(event)
+                }/>
+                <button onClick={onAddTaskHandle}>+</button>
+                <button onClick={props.onClear}>CLEAR
+                </button>
             </div>
             <ul>
-                {props.task.map((item, index) => {
-                    return(
-                        <li>
-                            <input defaultChecked={item.isChecked} key={index} type={"checkbox"} checked={item.isChecked}/>
+                {props.task.length > 0 ? props.task.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <input type={"checkbox"}
+                                   checked={item.isChecked} onChange={() => props.checkedTaskChange(item.id)}/>
                             <span>{item.title}</span>
-                            <button onClick={() => {props.onDelete(item.id)}}>x</button>
+                            <button onClick={() => {
+                                props.onDelete(item.id)
+                            }}>x
+                            </button>
                         </li>
                     )
-                })}
+                }) : <p>There are no tasks</p>}
             </ul>
             <div>
-                <button onClick={() => {props.all()}}>All</button>
-                <button onClick={() => {props.active()}}>Active</button>
-                <button onClick={() => {props.complete()}}>Complete</button>
+                <button className={`${props.filtered === 'all' ? 'button-active' : 'button'}`} onClick={props.all}>All
+                </button>
+                <button className={`${props.filtered === 'active' ? 'button-active' : 'button'}`} onClick={props.active}>Active
+                </button>
+                <button className={`${props.filtered === 'completed' ? 'button-active' : 'button'}`} onClick={props.complete}>Complete
+                </button>
             </div>
         </div>
 
